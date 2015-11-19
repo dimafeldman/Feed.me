93d5b86d995c64ad1d64c4406ad885fc3178e0ac
Eli Livshitz <eli.livshitz@gmail.com>
2015-11-19 18:10:40 +0200

    + hardcoded deals

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,28 @@ app.name = 'FeedMe';
 require('/js/routes.js').set(app);
 
 app.data = {};
+app.data.deals = [];
+
+app.newSubscriber = function(userId, username) {
+	return {
+		userId: userId,
+		name: username,		
+	}
+}
+app.newDeal = function() {
+	return {
+		title: 'שאריות של פלאפל שלומי',
+		price: '10',
+		image: '/img/deal1.jpg',
+		location: 'תל אביב, המסגר 35',
+		discount: '80%',
+		description: 'פלאפל טעים טעים',
+		quantity: 10,
+		created: Date.now(),
+		// new
+		subscribers: [app.newSubscriber('1', 'Shlomi'), app.newSubscriber('2', 'Kobi')],
+	}
+} 
 
 app.controller('layout', function ($scope, $rootScope, $location, page, utils, $timeout, $mdSidenav, $mdUtil, $log, $mdDialog) {
 	console.log('LayoutCtrl');
@@ -20,11 +42,16 @@ app.controller('layout', function ($scope, $rootScope, $location, page, utils, $
 
 app.controller('main', function($scope, $http) {
 	console.log('main');
+	
+	$scope.deals = [app.newDeal()]; 
+	
+	/*
 	$http.get('/getData')
 		.success(function (data) {
 			console.log('browse: success, app:', data);
 		}
 	);
+	*/
 });
 
 app.controller('AppCtrl', function($scope) {
